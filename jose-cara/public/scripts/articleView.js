931762203b2f5054d0ea9c1518690feef92ed916
cara ottmar <cara.ottmar@gmail.com>
2018-03-26 12:59:30 -0700

    as is

--- jose-cara/public/scripts/articleView.js
+++ jose-cara/public/scripts/articleView.js
@@ -3,8 +3,11 @@ var app = app || {};
 
 // TODO: Wrap the contents of this file, except for the preceding 'use strict' and 'var app...' declararions, in an IIFE.
 // Give the IIFE a parameter called 'module'.
-// At the very end of the code, but still inside the IIFE, attach the 'articleView' object to 'module'.
-// Where the IIFE is invoked, pass in the global 'app' object that is defined above.
+
+(function(module) {
+
+// DONE At the very end of the code, but still inside the IIFE, attach the 'articleView' object to 'module'.
+// DONE Where the IIFE is invoked, pass in the global 'app' object that is defined above.
 // Keep in mind that all references to 'Article' in this file now need to be renamed to 'app.Article'. There are not separate instructions for those; you'll need to debug and find them on your own.
 
 var articleView = {};
@@ -122,7 +125,7 @@ articleView.submit = event => {
 
   // REVIEW: The following line of code redirects the user back to the home page after submitting the form.
   window.location = '../';
-}
+};
 
 articleView.initIndexPage = () => {
   app.Article.all.forEach(a => $('#articles').append(a.toHtml()));
@@ -140,14 +143,15 @@ articleView.initAdminPage = () => {
   // TODO: Call the Handlebars .compile() method, which will return a function for you to use where needed.
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since we are then calling "template" below.
 
-    var template = Handlebars.compile($('#stats-template').text());
+    var template = Handlebars.compile($('#author-template').text());
+    return template(this);
 
-  };
   // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
   app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
   // REVIEW: Simply write the correct values to the page:
   $('#blog-stats .articles').text(app.Article.all.length);
   $('#blog-stats .words').text(app.Article.numWordsAll());
-  return template(this);
-};
+  };
+  module.articleView = articleView
+})(app);
--- jose-cara/server.js
+++ jose-cara/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // TODO: Don't forget your conString!!!
-const conString = 'postgres://postgres:1234@localhost:5432/kilovolt';
+const conString = 'postgres://localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => {
